5103b42f2ef23463aaf71e3256c3e7e288cba92a
Andrew Wood <43380910+awood50@users.noreply.github.com>
2020-04-15 15:01:08 -0400

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,40 @@
+﻿/*Name: Andrew Wood
+ *Date: 2/5/19
+ *Purpose: Car class in order to calculate acceleration and braking
+ * 
+ * 
+ */
+using System;
+using static System.Console;
+namespace CarClass
+{
+    public class Car
+    {
+        public int year_model; //public variable for the year model for the car
+        public string make; //public variable for the make of the car
+        public int speed; //public variable to hold the speed of the car
+
+        public void SetMake(string carMake, int carModel) //method to set the attributes of the car and set the speed to 0
+        {
+            year_model = carModel;
+            make = carMake;
+            speed = 0;
+        }
+
+        public int Accelerate() //Accelerate method that increases the speed and returns the value
+        {
+            speed = speed + 5; //adds 5 to the speed
+            return speed; //returns speed value
+        }
+
+        public int Brake()//Brake method that decreases the speed of the car
+        {
+            return speed = speed - 5; //Returns the value of the speed subtracted by 5
+        }
+
+        public int current_speed() //Method that gets the speed of the car
+        { 
+            return speed; //returns the speed of the car 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿/*Name: Andrew Wood
+ *Date: 2/22/19
+ *Purpose: Simulates car traveling speeds by running instances of Car class
+ * 
+ */
+
+using System;
+using static System.Console;
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();//creates a new instance of a car from the Car class
+
+
+            car.SetMake("Honda", 2014); //calls the SetMake method in order to create the make of the car
+
+            WriteLine("Begnning speed of the car: " + car.current_speed());//Prints the starting speed of the car, calls the current_speed method to display the speed of the car
+
+            for(int i = 0; i < 5; i++)//I created a for loop instead of having to call each method five times individually. Seemed easier so I hope that is okay 
+            {
+                WriteLine("Acceleration in progress."); //Prints a message to show acceleration
+                car.Accelerate(); //Calls the acceleration method from Car class
+                WriteLine("Current speed of the car: " + car.current_speed()); //Prints the current speed of the car
+
+            } 
+
+
+            for(int j = 0; j < 5; j++)//for loop that makes the car brake five times
+            {
+                WriteLine("Braking in progress.");//Shows that the car is braking
+                car.Brake();//Calls the brake method from the car class to slow the car down
+                WriteLine("Current speed of the car: " + car.current_speed()); //Prints the current speed of the car
+            } 
+
+
+
+            WriteLine("End of program"); //Just a message to show the completion of the program
+        }
+    }
+}
